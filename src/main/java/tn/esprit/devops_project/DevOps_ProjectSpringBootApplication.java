9565128291c94018e8c1fbe1cb24492ce1e0a52c
SkanderKasmi <iskander.kasmi@esprit.tn>
2023-11-07 12:28:36 +0100

    final test final 2

--- src/main/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplication.java
+++ src/main/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplication.java
@@ -1,7 +1,10 @@
 package tn.esprit.devops_project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DevOps_ProjectSpringBootApplication {
@@ -9,5 +12,8 @@ public class DevOps_ProjectSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(DevOps_ProjectSpringBootApplication.class, args);
     }
-
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
